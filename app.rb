f4dd46b04650433623139a21eb40f8646ca71a1d
Milind <milind@hashcooki.es>
2014-06-04 18:02:20 +0530

    Setting up models

--- app.rb
+++ app.rb
@@ -1,11 +1,29 @@
 require 'rubygems'
 require 'sinatra'
-require 'datamapper'
+require 'data_mapper'
+require 'sass'
+
+configure :development do
+	require 'dm-sqlite-adapter'
+	DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/products.db")
+end
+
+class Property
+	include DataMapper::Resource
+	
+	property :id,		Serial
+	property :title,	String
+	property :desc,		String
+end
+
+DataMapper.auto_upgrade!
 
 get '/' do
-  erb 'Are you ready for an adventure?'
+	@property = Property.get(1)
+	erb :home
 end
 
 get 'properties' do
-	erb 'Properties'
+	erb :home
 end
+
