ffef508b20ff4f8740cf40661de64d2d34f00694
Milind <milind@hashcooki.es>
2014-07-12 18:50:31 +0530

    Setting up gallery slideshow

--- public/js/main.js
+++ public/js/main.js
@@ -222,4 +222,32 @@ $(document).ready(function() {
 		$b.addClass('loaded');
 	}, 2000)
 	
+	$('.gallery-images').magnificPopup({ 
+		delegate: 'a',
+		gallery: {
+		    // options for gallery
+		    enabled: true,
+		    preload: [0,2], // read about this option in next Lazy-loading section
+		  },
+		type: 'image',
+		 zoom: {
+		    enabled: true, // By default it's false, so don't forget to enable it
+		
+		    duration: 300, // duration of the effect, in milliseconds
+		    easing: 'ease-in-out', // CSS transition easing function 
+		
+		    // The "opener" function should return the element from which popup will be zoomed in
+		    // and to which popup will be scaled down
+		    // By defailt it looks for an image tag:
+		    opener: function(openerElement) {
+		      // openerElement is the element on which popup was initialized, in this case its <a> tag
+		      // you don't need to add "opener" option if this code matches your needs, it's defailt one.
+		      return openerElement.is('img') ? openerElement : openerElement.find('img');
+		    }
+		  }
+	});
+	$('.gallery-link button').click(function() {
+		$('.gallery-images').magnificPopup('open');
+		return false;
+	});
 });
