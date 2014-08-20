e76ed9f2a5145fec0314aa113a757113b38cdaae
Milind <milind@hashcooki.es>
2014-08-21 04:00:00 +0530

    Filter stucking

--- public/js/main.js
+++ public/js/main.js
@@ -404,8 +404,18 @@ $(document).ready(function() {
 		  }
 		});
 		
-		$('#footer').waypoint(function() {
-			console.log('test');
-		}, { offset: 1000 });
+		$('#footer').waypoint(function(direction) {
+			var st = $(window).scrollTop();
+			console.log(st);
+			if (direction == "down") {
+				$('#filters').css({
+					top: st - 50,
+					position: 'absolute'
+				});
+			}
+			if (direction == "up") {
+				$('#filters').removeAttr('style');
+			}
+		}, { offset: 715 });
 	});
 });	
