f95c9cac1e2c2ccced62cafaf30e55699ee60156
Milind <milind@hashcooki.es>
2014-08-24 04:50:56 +0530

    Writing simple tests

--- app_spec.rb
+++ app_spec.rb
@@ -7,9 +7,27 @@ RSpec.configure do |config|
   config.include Capybara::DSL
 end
 
-describe 'Front End' do
+describe 'Visiting home page' do
 	it "Should have homepage content" do
 		visit '/'
 		page.should have_content('Real Estate')
+	end
+end
+
+describe "visiting services page" do
+	it "should have required content" do
+		visit '/about'
+		page.should have_content('Hassle-free Real Estate')
+		page.should have_button('Search')
+	end
+end
+
+describe "Searching for property" do
+	before do
+		visit '/'
+		first(:button, "Search").click
+	end
+	it "should load the search page" do
+		page.should have_content("Locations")
 	end
 end
