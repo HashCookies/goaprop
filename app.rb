1390a549916bd62c963629cd64ea049f757b0fc7
Milind <milind@hashcooki.es>
2014-10-12 03:01:26 +0530

    Aligning code

--- app.rb
+++ app.rb
@@ -639,7 +639,6 @@ get '/search' do
 	
 	@locations = @region.locations(:order => [:name.asc])
 	@properties = @locations.propertys(:state_id => @state.id, :is_active => true, :order => [:status.asc])
-	# with a sell or rent flag
 	
 	if @category.name != "All"
 		@properties = @properties.all(:category_id => @category.id) # selecting "Residential", "Commercial", etc
