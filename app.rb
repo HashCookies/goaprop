796b543d9e4955721bde109e6ff7f6a397c17a21
Ravi Siddarth <ravisiddarth13@gmail.com>
2015-02-13 17:14:46 +0530

    mobile layout changes

--- app.rb
+++ app.rb
@@ -18,7 +18,7 @@ end
 
 configure :development do
 	require 'dm-sqlite-adapter'
-	DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/db.db")
+	DataMapper::setup(:default, "sqlite3:db.db")
 end
 
 configure :test do
