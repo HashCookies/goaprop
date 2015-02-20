0c84e020f8668ae2979b590e1bb6635a9559e50a
Ravi Siddarth <ravisiddarth13@gmail.com>
2015-02-20 20:07:16 +0530

    tumblr blog testing

--- app.rb
+++ app.rb
@@ -765,67 +765,161 @@ post '/mail-sell-lease' do
 end
 
 # tumblr initialization 
-client = Tumblr::Client.new({
-  	:consumer_key => 'pQzvSIuaXKfGAfHo86uBIKCnw1rapysNYOgGFj6FL5xTmXpV0H',
-  	:consumer_secret => 'coTHJmyXvoqu4ypJ0JkYVa9aSpsT5HLm4lcQnRYa2IwKTzGRjw',
-  	:oauth_token => '8MmIIh1UkSe7soTPpNtzzXiI546q8evN2WHTgwICgAMWA6BmFM',
-  	:oauth_token_secret => 'JMeH1w98lWHhlLTDPuBWHtLG1eEJzbGr1sSMvIUTX1Z6SWJhQv'
-})
+# client = Tumblr::Client.new({
+#   	:consumer_key => 'pQzvSIuaXKfGAfHo86uBIKCnw1rapysNYOgGFj6FL5xTmXpV0H',
+#   	:consumer_secret => 'coTHJmyXvoqu4ypJ0JkYVa9aSpsT5HLm4lcQnRYa2IwKTzGRjw',
+#   	:oauth_token => '8MmIIh1UkSe7soTPpNtzzXiI546q8evN2WHTgwICgAMWA6BmFM',
+#   	:oauth_token_secret => 'JMeH1w98lWHhlLTDPuBWHtLG1eEJzbGr1sSMvIUTX1Z6SWJhQv'
+# })
 
-$blog_name = 'goapropco.tumblr.com'
-$posts_title = []
-$posts_body = []
-$posts_id = []
-$posts_slug = []
-$posts_categories = []
-$posts = client.posts($blog_name)
-$posts_count = $posts['blog']['posts']
-$count = 0
+# $blog_name = 'goapropco.tumblr.com'
 
+# #list all posts from tumblr account
+# $posts_title = []
+# $posts_body = []
+# $posts_id = []
+# $posts_slug = []
+
+# #posts categories
+# $articles = []
+# $a_id = []
+
+# $knowledge = []
+# $k_id = []
+
+# $news = []
+# $n_id = []
+# #fetch posts from tumblr
+# $posts = client.posts($blog_name)
+# $posts_count = $posts['blog']['posts']
+# $count = 0
+helpers do 
+	def posts 
+		posts = [
+					{    
+						'id' => '0',
+						'title' => 'test',
+						'body' => 'something something',
+						'tags' => 'articles',
+						'slug' => 'first'
+ 					},
+
+					{  	
+						'id' => '1',
+						'title' => 'test2',
+						'body' => 'something3 something2',
+						'tags' => 'news',
+						'slug' => 'second'
+					},
+
+					{    
+						'id' => '2',
+						'title' => 'test3',
+						'body' => 'something3 something3',
+						'tags' => 'knowledge',
+						'slug' => 'third'
+					},
+					{    
+						'id' => '3',
+						'title' => 'test4',
+						'body' => 'something3 something3',
+						'tags' => 'knowledge',
+						'slug' => 'fourth'
+					}
+
+			]
+	end
+
+	def articles
+		articles = []
+	end
+	
+	def knowledge
+		knowledge = []
+	end
+
+	def news
+		news = []
+	end
+end
 get '/blog' do
 	
 	@classes=['blog']
 	@properties = Property.all(:limit => 5)
 	@title ='Blog'
+	@news = news
+	@articles = articles
+	@knowledge = knowledge
+	@blog = posts
+			
+	@blog.each do |post|
 
-	@posts = $posts
-	@posts_count = $posts_count
-	@posts_title = $posts_title
-	@posts_body = $posts_body
-	@posts_slug = $posts_slug
-	@posts_categories = $posts_categories
-	count = 0 
-	@p_count = count
+		if (post['tags'] == 'news')
+			
+			@news << post
 
-	@posts_count.to_i.times do
-
-		@posts_title[count] = @posts['posts'][count]['title']
-		@posts_body[count] = @posts['posts'][count]['body'] 
-		@posts_slug[count] = @posts['posts'][count]['slug']
+		elsif (post['tags'] == 'knowledge')
 		
-		@posts_categories[count] = @posts['posts'][count]['tags'][0]
-		
-		# if(@posts_categories[count] == 'news') 
+			@knowledge << post
 
-		# end
-
-		count += 1	
-
+		elsif (post['tags'] == 'articles')
+			@articles << post
+		end
 	end	
+	# @posts = $posts
+	# @posts_count = $posts_count
+	# @posts_title = $posts_title
+	# @posts_body = $posts_body
+	# @posts_slug = $posts_slug
+	# @posts_categories = $posts_categories
+	# @knowledge = $knowledge
+	# @articles = $articles
+	# @news = $news
+	# count = 0 
+	# @p_count = count
+
+	# @posts_count.to_i.times do
+
+	# 	@posts_title[count] = @posts['posts'][count]['title']
+	# 	@posts_body[count] = @posts['posts'][count]['body'] 
+	# 	@posts_slug[count] = @posts['posts'][count]['slug']
+		
+	# 	@posts_categories[count] = @posts['posts'][count]['tags'][0]
+	# 	if (@posts_categories[count] == 'articles')
+	# 		@articles[count] = @posts_title[count]
+	# 		# a_id[count] = count
+
+	# 	elsif (@posts_categories[count] == 'news') 
+	# 		@news[count] = @posts_title[count]
+
+	# 		# n_id[count] = count
+	# 	elsif (@posts_categories[count] == 'knowledge')
+	# 		@knowledge[count] = @posts_title[count]
+	# 	end
+
+	# 	count += 1	
+
+	# end	
 	
 	erb	:blog
 end
 
 get '/blog/:id/:slug' do
+	
 	@classes = ['blog']
-	@properties = Property.all(:limit => 5)
-	@p_count = params[:id].to_i
-	@posts_count = $posts['blog']['posts']
-	@posts_title = $posts_title[@p_count]
-	@posts_body = $posts_body[@p_count]
-
+	@properties = Property.all(:limit => 5) 
+	blog = posts
+	@post_title = blog[params[:id].to_i]['title']
+	@post_body = blog[params[:id].to_i]['body']
+	@post_tags = blog[params[:id].to_i]['tags']
+	@posts_categories = false
+	# @posts_count = $posts['blog']['posts']
+	# @posts_title = $posts_title[params[:id].to_i]
+	# @posts_body = $posts_body[params[:id].to_i]
+	
 	erb :_blog_post
 end
+
 
 load 'actions/route_region.rb'
 load 'actions/route_location.rb'
